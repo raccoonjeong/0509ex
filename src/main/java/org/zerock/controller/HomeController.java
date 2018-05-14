1ad464fe9a0bb218845db9ab833496e71380086f
Kimmj6467 <alswn6467@gmail.com>
2018-05-14 16:21:34 +0900

    hello!

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -22,6 +22,7 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 * hi!!
+	 * hello!!
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
